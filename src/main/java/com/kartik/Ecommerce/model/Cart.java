439ae48651f732867d722121d0e7d60a2eca254b
Kartik2057 <2021kuec2057@iiitkota.ac.in>
2023-11-12 03:19:47 +0530

    modified fetchtype for the set of cart items

--- src/main/java/com/kartik/Ecommerce/model/Cart.java
+++ src/main/java/com/kartik/Ecommerce/model/Cart.java
@@ -22,7 +22,7 @@ public class Cart {
     @JoinColumn(name="user_id",nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,fetch = FetchType.EAGER,orphanRemoval = true)
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,orphanRemoval = true)
     private Set<CartItem> cartItems = new HashSet<CartItem>();
 
     private int totalPrice;
